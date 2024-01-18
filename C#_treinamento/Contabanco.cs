e9a324163385ee5a8eabc53e938b1f5a1b4d6ce5
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-01-18 15:26:17 -0400

    atualização de aprendizagem de POO em C#

--- C#_treinamento/Contabanco.cs
+++ C#_treinamento/Contabanco.cs
@@ -1,1 +1,14 @@
-﻿
+﻿class ContaBancaria
+{
+    public float saldo;
+    public float indicador;
+    public string titular;
+    int senha;
+    public void ImprimirDados()
+    {
+        Console.WriteLine($" SALDO DA CONTA: {saldo}");
+        Console.WriteLine($" TITULAR DA CONTA: {titular}");
+        Console.WriteLine($" INDICADOR DE CONTA: {indicador}");
+    }
+
+}
--- C#_treinamento/obj/Debug/net8.0/C#_treinamento.AssemblyInfo.cs
+++ C#_treinamento/obj/Debug/net8.0/C#_treinamento.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_treinamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396e308ac52b4d32862dbe2cbf3c441f438c8eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9873b596df26394909ca11b59a71553657446d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_treinamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_treinamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
