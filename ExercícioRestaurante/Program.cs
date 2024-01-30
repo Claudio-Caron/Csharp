3d9a99565f0f2400ad8be981af90c82eeca0dae7
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-01-30 17:08:17 -0400

    aprimoramento do exercício

--- ExercícioRestaurante/Program.cs
+++ ExercícioRestaurante/Program.cs
@@ -29,15 +29,67 @@ class Pedido
 {
     public int Id { get;  }
     private float Valor { get; set; }   
-    public Pedido()
+    public Pedido(int id)
     {
-      
+        Id= id;
+        Valor = 0;  
     }
 
 }
 class Cardapio
 {
-    public bool Hamburguer {  get; set; }   
-    public bool 
 
+  
+    private Hamburguer Ham {  get; set; }  
+    private BatataFrita Bat {  get; set; }  
+
+    public Cardapio() { 
+        Ham = new Hamburguer();
+        Bat = new BatataFrita();
+    } 
+
+}
+    class BatataFrita
+    {
+        string nome;
+        int porcoes;
+        float valor;
+        bool disponivel;
+    }
+class Hamburguer
+{
+    public int Estoque { get; set; }    
+    string Nome { get;  } 
+    private int Quantidade { get; set; }
+    double ValorProduto { get; set; }
+    public bool Disponivel
+        {
+        get
+        {
+            return Disponivel;
+        }
+        set
+        {
+            if (Estoque >= Quantidade)
+            {
+                Disponivel = true;
+            }
+            else
+            {
+                Disponivel = false;
+            }
+        }
+        }
+    public Hamburguer()
+    {
+        Disponivel = true;
+        Nome = "Hamburguer";
+        ValorProduto =29.9;
+        Quantidade = 0;
+        Estoque = 0;
+    }
+    public void DisponibilidadeProduto()
+    {
+        Disponivel= false;
+    }
 }
