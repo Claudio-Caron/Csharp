0af1e82de98390062cdb3ec4fa1ffcfe5ac3f99f
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2024-08-17 12:57:09 -0400

    atualizando organização de pasta

---
+++ CSharpCode/Desafio/Program.cs
@@ -0,0 +1,51 @@
+﻿                                                using System;
+                                                using System.Collections.Generic;
+                                                using System.Linq;
+                                                using System.Text;
+                                                using System.Threading.Tasks;
+
+                                                namespace Ex3e4e5_04CAAL;
+
+                                                internal class Controler
+                                                {
+                                                    public void PrincípioDeControle()
+                                                    {
+                                                        int x = 1;
+                                                        while (x) { }
+                                                        Console.WriteLine("\t\t\t\tSISTEMA DE CADASTRO E MANIPULAÇÃO DE DADOS DE PESSOAS\n\n");
+                                                        Console.WriteLine(@"Escolha uma das Opcoes para Seguir:\n
+                                                                \t1 -> Novo Cadastro-Pessoa\n" +
+                                                            " \t2 -> Exibir Pessoas Cadastradas em Ordem\n" +
+                                                            " \t3 -> Buscar Pessoa(Por nome/idade)\n" +
+                                                            " \t4 -> Exibir Dados de uma pessoa" +
+                                                            " \t5 -> ManipularJson\n" +
+                                                            " \t0 -> ENCERRAR PROGRAMA");
+                                                        x = int.Parse(Console.ReadLine()!);
+                                                        switch (x)
+                                                        {
+                                                            case 1:
+
+                                                                break;
+                                                            case 2:
+
+                                                                break;
+                                                            case 3:
+
+                                                                break;
+                                                            case 4:
+
+                                                                break;
+                                                            case 5:
+
+                                                                break;
+                                                            case 0:
+                                                                Console.WriteLine("PROGRAMA ENCERRADO\n");
+                                                                break;
+                                                            default:
+                                                                Console.WriteLine("COMANDO NÃO RECONHECIDO\n");
+                                                                break;
+                                                        }
+                                                    }
+
+                                                }
+                                                }
---
+++ CSharpCode/Manipulacao_Arquivos/ComplementoProgram.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+
+partial class Program
+{
+    static void ConverterTexto_Objeto()
+    {
+        string way = Path.GetFullPath("contas.txt");
+        using (FileStream arquivo = new FileStream(way, FileMode.Open))
+        {
+            using (StreamReader leitor = new StreamReader(arquivo))
+            {
+                
+                string linha;
+                
+                while (!leitor.EndOfStream)
+                {
+                    linha = leitor.ReadLine()!;
+                    string[] dados = linha.Split(',');
+                }
+            }
+        }
+    }
+    static void EscreverEmArquivo(string way, string dados)
+    {
+        using (FileStream arquivo = new FileStream(way, FileMode.Create))
+        {
+            using (StreamWriter escritor = new StreamWriter(arquivo)) 
+            {
+                escritor.WriteLine(dados);
+                escritor.Flush();
+            }
+        }
+    }
+    static void EscreverArquivoXml(string way, string dados)
+    {
+        try
+        {
+            string xml = XmlConvert.DecodeName(dados);
+            File.WriteAllText(way, xml);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
